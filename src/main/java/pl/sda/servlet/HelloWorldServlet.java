9a4c90a5e67c9548fc1ddd04f39c973f04d09bdc
lkre <lukasz.res@gmail.com>
2019-10-26 13:23:25 +0200

    zadanie 25

--- src/main/java/pl/sda/servlet/HelloWorldServlet.java
+++ src/main/java/pl/sda/servlet/HelloWorldServlet.java
@@ -1,18 +1,20 @@
 package pl.sda.servlet;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-//@WebServlet(name = "mojaNazwwaa"
-//        , urlPatterns = {"/", "/hello"},
-//        initParams = {
-//                @WebInitParam(name = "userId", value = "125456"),
-//                @WebInitParam(name = "localization", value = "Poland")
-//        }
-//)
+@WebServlet(name = "mojaNazwwaa"
+        , urlPatterns = {"/", "/hello"},
+        initParams = {
+                @WebInitParam(name = "userId", value = "125456"),
+                @WebInitParam(name = "localization", value = "Poland")
+        }
+)
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
