e72fb72ec04f70770be5395f82a364894499e8f3
lkre <lukasz.res@gmail.com>
2019-10-26 11:42:01 +0200

    zadanie 23

--- src/main/java/pl/sda/HelloWorldServlet.java
+++ src/main/java/pl/sda/HelloWorldServlet.java
@@ -1,14 +1,20 @@
 package pl.sda;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-//@WebServlet(name = "mojaNazwwaa"
-//        , urlPatterns = {"/", "/hello"}
-//)
+@WebServlet(name = "mojaNazwwaa"
+        , urlPatterns = {"/", "/hello"},
+        initParams = {
+                @WebInitParam(name = "userId", value = "125456"),
+                @WebInitParam(name = "localization", value = "Poland")
+        }
+)
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
@@ -22,7 +28,10 @@ public class HelloWorldServlet extends HttpServlet {
     public void doPost(HttpServletRequest request
             , HttpServletResponse response)
             throws IOException {
+        String userId = getServletConfig().getInitParameter("userId");
+        String localization = getServletConfig().getInitParameter("localization");
+
         PrintWriter writer = response.getWriter();
-        writer.write("Hello world!");
+        writer.write("Witaj " + userId + " z " + localization);
     }
 }
