8a18a57014f5a86c04a06d0c7e16b1ca8e95f55c
lkre <lukasz.res@gmail.com>
2019-10-26 11:31:14 +0200

    zadanie 22

--- src/main/java/pl/sda/HelloWorldServlet.java
+++ src/main/java/pl/sda/HelloWorldServlet.java
@@ -1,15 +1,14 @@
 package pl.sda;
 
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "mojaNazwwaa"
-        , urlPatterns = {"/"}
-)
+//@WebServlet(name = "mojaNazwwaa"
+//        , urlPatterns = {"/", "/hello"}
+//)
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
